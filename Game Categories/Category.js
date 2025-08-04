8b846eed0702640f4a5a3b196b373e8d466b0caf
Laro <laroernesto@gmail.com>
2025-08-04 21:00:32 +0800

    d

---
+++ Game Categories/Category.js
@@ -0,0 +1,22 @@
+function openCategory(evt, category) {
+
+    var i, navbarcontent, navbutton;
+
+    navbarcontent = document.getElementsByClassName("navbarcontent")
+    for (i=0; i < navbarcontent.length; i++) {
+        navbarcontent[i].style.display = "none";
+    }
+
+    navbutton = document.getElementsByClassName("navbutton");
+    for (i=0; i < navbutton.length; i++) {
+        navbutton[i].className = navbutton[i].className.replace(" active", "");
+    }
+
+    document.getElementById(category).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
+
+function gurt (){
+    alert("Hello, Gurt!");
+}
