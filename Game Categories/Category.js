7f389f56f53ad5a558834f6cfbe120c801465839
Laro <laroernesto@gmail.com>
2025-08-06 09:47:49 +0800

    ts so fire

--- Game Categories/Category.js
+++ Game Categories/Category.js
@@ -2,12 +2,12 @@ function openCategory(evt, category) {
 
     var i, navbarcontent, navbutton;
 
-    navbarcontent = document.getElementsByClassName("navbarcontent")
+    navbarcontent = document.getElementsByClassName("tabbarcontent")
     for (i=0; i < navbarcontent.length; i++) {
         navbarcontent[i].style.display = "none";
     }
 
-    navbutton = document.getElementsByClassName("navbutton");
+    navbutton = document.getElementsByClassName("tabbutton");
     for (i=0; i < navbutton.length; i++) {
         navbutton[i].className = navbutton[i].className.replace(" active", "");
     }
